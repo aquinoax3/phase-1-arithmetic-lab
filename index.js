0b3c5d50205ee87468bfef2f34d4039615eeed23
Axel Aquino <aquino.axel@gmail.com>
2022-01-30 22:32:12 -0500

    commit tree ec703e4a9d2c6f57a3cb50a2557d70dccb90f292

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * 2;
+
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 74;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(2,3,20)
